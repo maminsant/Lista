614842574080aa49940f18737305d9f86b70ba8c
20211TIIMI0141 <mamin8172@gmail.com>
2023-03-31 09:13:27 -0300

    layout

---
+++ app/src/main/java/yasmin/santana/rodrigues/lista/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package yasmin.santana.rodrigues.lista.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import yasmin.santana.rodrigues.lista.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/yasmin/santana/rodrigues/lista/activity/NewItemActivity.java
+++ app/src/main/java/yasmin/santana/rodrigues/lista/activity/NewItemActivity.java
@@ -1,14 +1,16 @@
-package yasmin.santana.rodrigues.lista;
+package yasmin.santana.rodrigues.lista.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import yasmin.santana.rodrigues.lista.R;
+
+public class NewItemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_item);
     }
 }
